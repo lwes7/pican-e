6982d6804b83f842f7693e6ba455be3781ba30c4
lwes7 <galaxy7giraffe@gmail.com>
2019-04-27 18:47:31 +0100

    update

--- bot.js
+++ bot.js
@@ -2,6 +2,11 @@ var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
